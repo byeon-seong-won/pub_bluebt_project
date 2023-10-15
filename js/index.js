a3931013f82def0844b1327f4b066215644b507a
seong <sngoneee@gmail.com>
2023-10-16 00:18:10 +0900

    스크립트 수정

--- js/index.js
+++ js/index.js
@@ -183,11 +183,11 @@ $(document).ready(function () {
 
 
   // 브라우저 resize 처리
-  $(window).scroll(function() {
-    window.addEventListener("resize", function() {
-      $(window).scrollTop(0);
-    })
-  })
+  // $(window).scroll(function() {
+  //   window.addEventListener("resize", function() {
+  //     $(window).scrollTop(0);
+  //   })
+  // })
 
 
 
